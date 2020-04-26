dd785031b3a165cf7222972027fc8e71ee488a04
Matt Benson <rabenson6@msn.com>
2020-04-25 21:32:40 -0700

    working sort

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -10,31 +10,54 @@ class App extends Component {
   // Setting this.state.friends to the employees json array
   constructor(){
     super();
-
+    this.toggleSortDate = this.toggleSortDate.bind(this)
     this.state={
       search:null,
       result: employees
     };
   }
+  sortByDate() {
+    let employeeList = employees;
+    let sortedemployees = employeeList.sort((a, b) => (a.registered.age > b.registered.age)? 1 : -1);
+    this.setState({
+     result: sortedemployees
+    })
+  }
+  toggleSortDate (event) {
+    this.sortByDate()
+  }
+  revertList(event) {
+    let originalList = employees;
+    this.setState({
+      result: originalList
+    })
+    console.log(originalList)
+  }
+  toggleRevert(event) {
+    this.revertList()
+    console.log("click!@!@@!@!")
+    }
+  
   searchSpace=(event)=>{
     let keyword = event.target.value;
     this.setState({search:keyword})
   }
-  // componentDidMount() {
-  //   this.searchEmployees("");
-  // }
+ 
 
 
 
-  render() {
-      
-    
+
+  render() {  
     return (
       <Wrapper>
         <Title>Employee List
         <div>
       <input type="text" placeholder="search for employees" onChange={(e)=>this.searchSpace(e)} />
+      <br></br>
+      <button onClick = {(e)=>this.toggleSortDate(e)} >Sort by Years of Service</button>
+      <button onClick = {(e)=>this.toggleRevert(e)} >Revert</button>
       </div>
+      
         </Title>
         {this.state.result.filter((data)=>{
           if(this.state.search == null)
@@ -43,7 +66,6 @@ class App extends Component {
           return data
       }
     })
-        
         .map(employee => (
           <EmployeeCard 
             name={employee.name.first+" "+employee.name.last}
